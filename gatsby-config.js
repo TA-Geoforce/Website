c4fa996d178dabe32e33c88c68e4b4af08d71ce8
Christos Charmatzis <xr_xarmatzis@outlook.com>
2019-05-04 15:45:05 +0300

    update to natural disaster loss app

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'ta-geoforce-website',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#000000',
+        theme_color: '#000000',
         display: 'minimal-ui',
         icon: 'src/images/global1.png', // This path is relative to the root of the site.
       },
